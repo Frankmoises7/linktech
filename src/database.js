20d5150409987503e6c4b3b3e137bc2bcf792953
Frank Islas <frank.islas.zuniga@ciisa.cl>
2023-04-27 11:21:07 -0400

    fix pckg.json

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { mongodb } = require('./keys');
 
 
-const uri = `mongodb+srv://${process.env.USERDB}:${process.env.PASSWORD}@cluster0.empmxhu.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
+const uri = mongodb.URI
 const option = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.connect(uri, option)
     .then(() => console.log('Base de datos conectada'))
--- src/keys.js
+++ src/keys.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 module.exports = {
   mongodb: {
-    URI: `mongodb+srv://${process.env.USERDB}:${process.env.PASSWORD}@cluster0.empmxhu.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
+    URI: `mongodb+srv://${process.env.USERDB}:${process.env.PASSWORD}@cluster0.empmxhu.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`,
   }
 };
